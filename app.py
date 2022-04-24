d5b8474aec4332d19062e1407c5b6aaac61ffe1b
Jennifer Lane <jennifer.lane610@gmail.com>
2022-04-24 16:58:13 +0100

    Added relevant code to base.html and index.html files; wrote mainpage route in app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from controllers.animals_controller import animals_blueprint
-from controllers.practices_controller import practices_blueprint
+from controllers.owners_controller import owners_blueprint
 from controllers.vets_controller import vets_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(animals_blueprint)
-app.register_blueprint(practices_blueprint)
+app.register_blueprint(owners_blueprint)
 app.register_blueprint(vets_blueprint)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
