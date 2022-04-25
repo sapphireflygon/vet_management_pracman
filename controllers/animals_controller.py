a9adce7bb6ed34045e061b8aca3f02064d658dcd
Jennifer Lane <jennifer.lane610@gmail.com>
2022-04-25 09:28:04 +0100

    Found error, silly @ missing at start of animals_blueprint.route! Working now woo

--- controllers/animals_controller.py
+++ controllers/animals_controller.py
@@ -8,7 +8,7 @@ import repositories.vet_repo as vet_repo
 animals_blueprint = Blueprint("animals", __name__)
 
 # animals.html
-animals_blueprint.route("/animals")
+@animals_blueprint.route("/animals")
 def animals():
     animals = animal_repo.select_all()
     return render_template("animals/animals.html", animals=animals)
